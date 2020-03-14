6b3ad6ebaf5ecf1bbf59a3e100e0d1d4781b01b1
KjeilOlson <kjeilolson87@gmail.com>
2020-03-14 18:39:02 -0500

    addition of script logic, and needed packages

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 import logging 
+import RPi.GPIO as GPIO
+import time
+import sys
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- app/routes.py
+++ app/routes.py
@@ -13,7 +13,32 @@ def index():
   # app.logger.critical('this is a CRITICAL message')
   form = FeedForm() 
   if form.validate_on_submit():
-      app.logger.info('this is in the form submit')
+      def feed():
+    # let the GPIO library know where we've connected our servo to the Pi
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(12, GPIO.OUT)
+    attempts = int(sys.argv[1])
+
+    try:
+        servo = GPIO.PWM(12, 50)
+        servo.start(12.5)
+
+        # spin left, right, then left again rather than in a continuous circle
+        # to prevent the food from jamming the servo
+        for index in range(0, 3):
+            dutyCycle = 2.5 if (index % 2 == 0) else 12.5
+            servo.ChangeDutyCycle(dutyCycle)
+            # adjust the sleep time to have the servo spin longer or shorter in that direction
+            time.sleep(.75)
+    finally:
+        # always cleanup after ourselves
+        servo.stop()
+        GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    # kick off the feeding process (move the servo)
+    feed()
   return render_template('index.html', title='Cat-o-matic', username=username, form=form )
 
   # @app.route('/feed', methods=['POST'])
