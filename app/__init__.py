fae58f153219f8cf1dafd98675b2b4b9e28f85cd
SirCaptainMitch <hamann.mitchell@gmail.com>
2020-03-14 16:51:01 -0500

    adding logic for a form to start the feed action

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+import logging 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.logger.setLevel(logging.INFO)
 
 from app import routes
 
